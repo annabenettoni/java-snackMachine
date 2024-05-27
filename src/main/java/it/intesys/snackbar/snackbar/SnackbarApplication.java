882e4c2b4faf46c35746c1f033530b28bf73958c
annabenettoni <annabenettoni@gmail.com>
2024-05-27 09:53:11 +0200

    Initial commit

---
+++ src/main/java/it/intesys/snackbar/snackbar/SnackbarApplication.java
@@ -0,0 +1,13 @@
+package it.intesys.snackbar.snackbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackbarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/intesys/snackbar/snackbar/SnackbarApplicationTests.java
@@ -0,0 +1,13 @@
+package it.intesys.snackbar.snackbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackbarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
